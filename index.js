6f7c01924f07d07e46ccb177606655493ba19fa2
David Miranda <dvdfermiranda@yahoo.com.mx>
2024-06-11 23:49:49 -0600

    Added feature to create the README file using Inquirer prompts

--- index.js
+++ index.js
@@ -1,13 +1,74 @@
-// TODO: Include packages needed for this application
+// Import packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of question objects for inquirer.prompt()
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the project title?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the project description?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation steps?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage information?'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What are the contribution guidelines?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the instructions for testing?'
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose a license.",
+        choices: ["Apache 2.0", "Boost", "GNU GPLv3", "MIT", "Mozilla", "Unlicense", "Other"]
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+    }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function that writes the README file
+function writeToFile(fileName, data) {
+    const content = generateMarkdown(data);
+    fs.writeFile(fileName, content, (err) => {
+        err ? console.error(err) : console.log('README file created.');
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            writeToFile('./result/README.md', response);
+        });
+}
 
 // Function call to initialize app
 init();
---
+++ result/README.md
@@ -0,0 +1,33 @@
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+# 1
+
+## Description
+2
+
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contributing](#contributing)
+5. [Tests](#tests)
+6. [Questions](#questions)
+
+## Installation
+3
+
+## Usage
+4
+
+## License
+Apache 2.0: https://opensource.org/licenses/Apache-2.0
+
+## Contributing
+5
+
+## Tests
+1
+
+## Questions
+- Github: [1](https://github.com/1)
+- Email: 2
+  
